7912130bec8a953bc2bf9ca1ad693acc56a5de8b
Bernardo Orozco Garza <Infinity0106@users.noreply.github.com>
2017-09-07 20:40:30 -0500

    Update react-native-highcharts.js
    same file of App/index.js

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -1,64 +1,91 @@
 import React, { Component, PropTypes, } from 'react';
-import { AppRegistry, StyleSheet, Text, View, WebView, Image, Dimensions } from 'react-native';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    WebView,
+    Image,
+    Dimensions
+} from 'react-native';
 
-const { height, width } = Dimensions.get('window');
-
+const win = Dimensions.get('window');
 class ChartWeb extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      height,
-      width
+    constructor(props){
+        super(props);
+
+        this.state={
+            init:`<html>
+                    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0" />
+                    <style media="screen" type="text/css">
+                    #container {
+                        width:100%;
+                        height:100%;
+                        top:0;
+                        left:0;
+                        right:0;
+                        bottom:0;
+                        position:absolute;
+                    }
+                    </style>
+                    <head>
+                        <script src="https://code.jquery.com/jquery-2.1.4.min.js"></script>
+                        ${this.props.stock ? '<script src="https://code.highcharts.com/stock/highstock.js"></script>'
+                                      : '<script src="https://code.highcharts.com/highcharts.js"></script>'}
+                        ${this.props.more ? '<script src="https://code.highcharts.com/highcharts-more.js"></script>'
+                                      : ''}
+                        <script src="https://code.highcharts.com/modules/exporting.js"></script>
+                        <script>
+                        $(function () {
+                            Highcharts.${this.props.stock ? 'stockChart' : 'chart'}('container', `,
+            end:`           );
+                        });
+                        </script>
+                    </head>
+                    <body>
+                        <div id="container">
+                        </div>
+                    </body>
+                </html>`,
+            Wlayout:{
+                height:win.height,
+                width:win.width
+            }
+        }
     }
-  }
-  createChartHTML(config) {
-    return `<html>        
-        <head>
-          <style media="screen" type="text/css">#container{width:100%;height:100%;top:0;left:0;right:0;bottom:0;position:absolute;}</style>
-            <script src="https://code.highcharts.com/${this.props.stock ? 'stock/highstock' : 'highcharts'}.js"></script>
-            <script src="https://code.highcharts.com/modules/exporting.js"></script>
-            <script>
-              document.addEventListener("DOMContentLoaded", function() {
-                Highcharts.${this.props.stock ? 'stockChart' : 'chart'}('container', ${config});
-              });
-            </script>
-        </head>
-        <body>
-            <div id="container" />
-        </body>
-    </html>`
-  }
-  // used to resize on orientation of display
-  reRenderWebView(e) {
-    this.setState({
-      height: e.nativeEvent.layout.height,
-      width: e.nativeEvent.layout.width,
-    })
-  }
 
-  render() {
-    const config = JSON.parse(JSON.stringify(this.props.config, function (key, value) {
-      //create string of json but if it detects function it uses toString()
-      return (typeof value === 'function') ? value.toString() : value;
-    }));
+    // used to resize on orientation of display
+    reRenderWebView(e) {
+        this.setState({
+            height: e.nativeEvent.layout.height,
+            width: e.nativeEvent.layout.width,
+        })
+    }
 
-    const concatHTML = this.createChartHTML(flattenObject(config));
+    render() {
+        let config = JSON.stringify(this.props.config, function (key, value) {//create string of json but if it detects function it uses toString()
+            return (typeof value === 'function') ? value.toString() : value;
+        });
 
-    return (
-      <View style={this.props.style}>
-        <WebView
-          onLayout={this.reRenderWebView}
-          style={styles.full}
-          source={{ html: concatHTML, baseUrl: 'web/' }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          scalesPageToFit={this.props.scaleToFit}
-          scrollEnabled={false}
-          automaticallyAdjustContentInsets={true}
-        />
-      </View>
-    );
-  };
+
+        config = JSON.parse(config)
+        let concatHTML = `${this.state.init}${flattenObject(config)}${this.state.end}`;
+
+        return (
+          <View style={this.props.style}>
+              <WebView
+                  onLayout={this.reRenderWebView}
+                  style={styles.full}
+                  source={{ html: concatHTML, baseUrl: 'web/' }}
+                  javaScriptEnabled={true}
+                  domStorageEnabled={true}
+                  scalesPageToFit={true}
+                  scrollEnabled={false}
+                  automaticallyAdjustContentInsets={true}
+              />
+          </View>
+        );
+    };
 };
 
 var flattenObject = function (obj, str='{') {
