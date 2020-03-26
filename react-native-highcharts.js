84423acb1ee507cf4693c4d3be23bd6dd08b369c
Ravi Rupareliya <rupareliyaravi@gmail.com>
2020-03-26 09:52:53 +0530

    Removed Webview import from react native

--- react-native-highcharts.js
+++ react-native-highcharts.js
@@ -4,10 +4,10 @@ import {
     StyleSheet,
     Text,
     View,
-    WebView,
     Image,
     Dimensions
 } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 const win = Dimensions.get('window');
 class ChartWeb extends Component {
